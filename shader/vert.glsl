356db4d6b141198405329d46c4fe4f8446dca460
Steven <95512683+sutibn@users.noreply.github.com>
2024-12-16 04:31:38 -0800

    Add color properties

--- shader/vert.glsl
+++ shader/vert.glsl
@@ -1,13 +1,11 @@
 #version 300 es
 
-in vec2 a_position;
-uniform vec2 u_resolution;
+in vec3 a_position;
+uniform mat4x4 u_m;
+uniform mat4x4 u_v;
+uniform mat4x4 u_p;
 
 void main() {
-    vec2 zeroToOne = a_position / u_resolution;
-    vec2 zeroToTwo = zeroToOne * 2.0;
-    vec2 clipSpace = zeroToTwo - 1.0;
-
-    gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
-    gl_PointSize = 10.0;
+    gl_PointSize = 2.0f;
+    gl_Position = u_p * u_v * u_m * vec4(a_position, 1.0);
 }
